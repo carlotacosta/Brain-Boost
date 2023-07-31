035b6be23d9fd740b6af322f9b911ee3fdc5334a
Carlota Costa <carlotasofiacosta@icloud.com>
2023-07-31 22:37:37 +0100

    Initial Commit

---
+++ Brain Boost/Brain_BoostApp.swift
@@ -0,0 +1,17 @@
+//
+//  Brain_BoostApp.swift
+//  Brain Boost
+//
+//  Created by Carlota Costa on 31/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Brain_BoostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
